e19f1f4740ac25c1720f1f43d580464a428d186b
goandreus <andres.chavez.crisostomo@gmail.com>
2021-05-30 19:54:43 -0500

    wather search page

---
+++ lib/pages/weather_search_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WeatherSearchPage extends StatelessWidget {
+  const WeatherSearchPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
